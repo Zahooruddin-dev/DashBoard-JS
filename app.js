8540d6631d9ea8fbe73d7af682c82f382619a853
Muhammad Zahooruddin Nizamani <84314402+Zahooruddin-dev@users.noreply.github.com>
2024-09-21 06:09:57 +0500

    cooderinates

--- app.js
+++ app.js
@@ -29,7 +29,30 @@ function getTime(){
   const date= new Date()
   const formattedTime  = date.toLocaleTimeString("en-us", {timeStyle: "short"})
   time.innerHTML=`${formattedTime}`
+
 }
+
+function getGeoLocation(){
+const options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0,
+};
+
+function success(pos) {
+  const crd = pos.coords;
+  console.log(`Latitude : ${crd.latitude}`);
+  console.log(`Longitude: ${crd.longitude}`);
+}
+
+function error(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+}
+
+navigator.geolocation.getCurrentPosition(success, error, options);}
+
+
+getGeoLocation()
 setInterval(getTime,1000)
 handleExchangeRates()
 handleImageGeneartion()
